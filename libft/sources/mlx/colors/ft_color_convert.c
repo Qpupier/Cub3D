9b8f926399f06e65d472eb401dd685bb50b743ec
Quentin PUPIER <qpupier@student.le-101.fr>
2021-05-14 17:16:00 +0200

    Fix RGB + transparency

--- libft/sources/mlx/colors/ft_color_convert.c
+++ libft/sources/mlx/colors/ft_color_convert.c
@@ -6,7 +6,7 @@
 /*   By: qpupier <qpupier@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/09 16:30:15 by qpupier           #+#    #+#             */
-/*   Updated: 2021/05/14 14:53:25 by qpupier          ###   ########lyon.fr   */
+/*   Updated: 2021/05/14 17:09:20 by qpupier          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ t_rgb	hex_to_rgb(unsigned int color)
 	return (c);
 }
 
-unsigned int	rgb_to_hex(t_rgb c)
+inline unsigned int	rgb_to_hex(t_rgb c)
 {
-	return (c.a << 24 | c.g << 16 | c.b << 8 | c.r);
+	return (c.a << 24 | c.r << 16 | c.g << 8 | c.b);
 }
